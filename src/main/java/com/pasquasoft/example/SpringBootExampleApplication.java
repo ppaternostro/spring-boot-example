d215271d8b256d772ecc9bf291145fd5a3e2bb8a
ppaternostro <ppaternostro@yahoo.com>
2018-03-01 21:10:09 -0500

    Source code formatting.

--- src/main/java/com/pasquasoft/example/SpringBootExampleApplication.java
+++ src/main/java/com/pasquasoft/example/SpringBootExampleApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootExampleApplication {
+public class SpringBootExampleApplication
+{
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootExampleApplication.class, args);
-	}
+  public static void main(String[] args)
+  {
+    SpringApplication.run(SpringBootExampleApplication.class, args);
+  }
 }
