5348f03487e3144512b72447c18ad21975b5d899
ppaternostro <ppaternostro@yahoo.com>
2018-03-01 20:48:08 -0500

    Initial commit

---
+++ src/main/java/com/pasquasoft/example/SpringBootExampleApplication.java
@@ -0,0 +1,12 @@
+package com.pasquasoft.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+}
