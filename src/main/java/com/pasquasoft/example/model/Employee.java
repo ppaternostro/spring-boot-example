4267f24c54cdfbf9c0862bc96897a9a5603fa03e
ppaternostro <ppaternostro@yahoo.com>
2018-03-02 18:23:34 -0500

    Initial model classes

---
+++ src/main/java/com/pasquasoft/example/model/Employee.java
@@ -0,0 +1,98 @@
+package com.pasquasoft.example.model;
+
+/**
+ * A class that defines the attributes and behavior of an employee.
+ * 
+ * @author ppaternostro
+ *
+ */
+public class Employee extends Person
+{
+  private Long id;
+  private String ssn;
+
+  /**
+   * Retrieves the id.
+   * 
+   * @return the id
+   */
+  public Long getId()
+  {
+    return id;
+  }
+
+  /**
+   * Sets the id.
+   * 
+   * @param id the id
+   */
+  public void setId(Long id)
+  {
+    this.id = id;
+  }
+
+  /**
+   * Retrieves the SSN.
+   * 
+   * @return the SSN
+   */
+  public String getSsn()
+  {
+    return ssn;
+  }
+
+  /**
+   * Sets the SSN.
+   * 
+   * @param ssn the ssn
+   */
+  public void setSsn(String ssn)
+  {
+    this.ssn = ssn;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = super.hashCode();
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    result = prime * result + ((ssn == null) ? 0 : ssn.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (!super.equals(obj))
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Employee other = (Employee) obj;
+    if (id == null)
+    {
+      if (other.id != null)
+        return false;
+    }
+    else if (!id.equals(other.id))
+      return false;
+    if (ssn == null)
+    {
+      if (other.ssn != null)
+        return false;
+    }
+    else if (!ssn.equals(other.ssn))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "Employee [id=" + id + ", ssn=" + ssn + ", "
+        + super.toString() + "]";
+  }
+
+}
---
+++ src/main/java/com/pasquasoft/example/model/Person.java
@@ -0,0 +1,128 @@
+package com.pasquasoft.example.model;
+
+/**
+ * A class that defines the attributes and behavior of a person.
+ * 
+ * @author ppaternostro
+ * 
+ */
+public abstract class Person
+{
+  private String lastName;
+  private String firstName;
+  private String middleName;
+
+  /**
+   * Retrieves the last name.
+   * 
+   * @return the last name
+   */
+  public String getLastName()
+  {
+    return lastName;
+  }
+
+  /**
+   * Sets the last name.
+   * 
+   * @param lastName the last name
+   */
+  public void setLastName(String lastName)
+  {
+    this.lastName = lastName;
+  }
+
+  /**
+   * Retrieves the first name.
+   * 
+   * @return the first name
+   */
+  public String getFirstName()
+  {
+    return firstName;
+  }
+
+  /**
+   * Sets the first name.
+   * 
+   * @param firstName the first name
+   */
+  public void setFirstName(String firstName)
+  {
+    this.firstName = firstName;
+  }
+
+  /**
+   * Retrieves the middle name.
+   * 
+   * @return the middle name
+   */
+  public String getMiddleName()
+  {
+    return middleName;
+  }
+
+  /**
+   * Sets the middle name.
+   * 
+   * @param middleName the middle name
+   */
+  public void setMiddleName(String middleName)
+  {
+    this.middleName = middleName;
+  }
+
+  @Override
+  public int hashCode()
+  {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+    result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+    result = prime * result
+        + ((middleName == null) ? 0 : middleName.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj)
+  {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    Person other = (Person) obj;
+    if (firstName == null)
+    {
+      if (other.firstName != null)
+        return false;
+    }
+    else if (!firstName.equals(other.firstName))
+      return false;
+    if (lastName == null)
+    {
+      if (other.lastName != null)
+        return false;
+    }
+    else if (!lastName.equals(other.lastName))
+      return false;
+    if (middleName == null)
+    {
+      if (other.middleName != null)
+        return false;
+    }
+    else if (!middleName.equals(other.middleName))
+      return false;
+    return true;
+  }
+
+  @Override
+  public String toString()
+  {
+    return "Person [lastName=" + lastName + ", firstName=" + firstName
+        + ", middleName=" + middleName + "]";
+  }
+
+}
