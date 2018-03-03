1254e62ba76bdd8e32519f7b64cf6647c2e9e7ee
ppaternostro <ppaternostro@yahoo.com>
2018-03-03 15:57:22 -0500

    Seed the in-memory database on application startup

--- src/main/java/com/pasquasoft/example/SpringBootExampleApplication.java
+++ src/main/java/com/pasquasoft/example/SpringBootExampleApplication.java
@@ -1,14 +1,36 @@
 package com.pasquasoft.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.pasquasoft.example.employee.EmployeeRepository;
+import com.pasquasoft.example.model.Employee;
 
 @SpringBootApplication
 public class SpringBootExampleApplication
 {
+  @Autowired
+  private EmployeeRepository employeeRepository;
 
   public static void main(String[] args)
   {
     SpringApplication.run(SpringBootExampleApplication.class, args);
+  }
+
+  /*
+   * Seed the in-memory database on application startup.
+   */
+  @Bean
+  CommandLineRunner runner()
+  {
+    return args -> {
+      employeeRepository.save(new Employee(1L, "Lennon"));
+      employeeRepository.save(new Employee(2L, "McCartney"));
+      employeeRepository.save(new Employee(3L, "Harrison"));
+      employeeRepository.save(new Employee(4L, "Starr"));      
+    };
   }
 }
